0196e0ace976005dbdadc932b0e6e294dae43bc4
Danielle Badobre <badobredanielle@gmail.com>
2023-11-08 23:06:49 +0000

    dude sleep please

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     delete_list = []
     for key, valu in a_dictionary.items():
-        if valu = value:
+        if valu == value:
             delete_list.append(key)
     for key in delete_list:
         del a_dictionary[key]
