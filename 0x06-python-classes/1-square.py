2b76b98b07182c601a5a0d1b9479896d0880af89
Danielle Badobre <badobredanielle@gmail.com>
2023-12-14 16:12:45 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""creation of class Square"""
+
+
+class Square:
+    """Attributes:
+        size: size of the square
+
+    """
+
+    def __init__(self, size):
+        """Args:
+            size: private attribut, square's size
+
+        """
+
+        self.__size = size
