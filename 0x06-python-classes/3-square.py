4c3303d51a1523f3c3d87ec325be99ac25dbd8df
Danielle Badobre <badobredanielle@gmail.com>
2023-12-14 17:36:45 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class square."""
+
+
+class Square:
+    """ class square """
+
+    def __init__(self, size=0):
+        """Args:
+            size (int): square's size
+        """
+
+        if type(size) is not int:
+            raise ValueError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ returns area of square"""
+        return (self.__size ** 2)
