4563b856dfab52f8c36fd77a8ef22a42edd69411
Danielle Badobre <badobredanielle@gmail.com>
2024-02-10 16:40:56 +0000

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that prints a text"""
+
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines after certain chars
+    :param text: text to print
+    :return: nothing
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    skip_space = False
+    for char in text:
+        if (char == "?") or (char == ":") or (char == "."):
+            print(char, end="\n\n")
+            skip_space = True
+        elif char == " " and skip_space:
+            skip_space = False
+        else:
+            print(char, end="")
