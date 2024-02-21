aaca57ec3ce9fc5878988d80b2dc3b73da3aba15
Danielle Badobre <badobredanielle@gmail.com>
2024-02-21 22:07:09 +0000

    fix inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,34 +2,10 @@
 """ Module for an empty class"""
 
 
-class BaseGeometry:
-    """ class basegeometry """
-    def area(self):
-        """ public instance method to raise exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Public instance method that validates value"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle(BaseGeometry):
-    """Class to create a rectangle"""
-    def __init__(self, width, height):
-        """initialization"""
-        self.__width = width
-        self.__height = height
-        self.integer_validator("height", self.__height)
-        self.integer_validator("width", self.__width)
-
-    def area(self):
-        return self.__height * self.__width
-
-    def __str__(self):
-        return (f"[Rectangle] {self.__width}/{self.__height}")
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,18 +2,7 @@
 """ Module for an empty class"""
 
 
-class BaseGeometry:
-    """ class basegeometry """
-    def area(self):
-        """ public instance method to raise exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Public instance method that validates value"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
