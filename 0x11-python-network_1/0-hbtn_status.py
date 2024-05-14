0e0bd1c629ae3554e1beb17af5d43e8477ece4b9
Danielle Badobre <badobredanielle@gmail.com>
2024-05-14 15:49:15 +0000

    task 0 added doc

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import urllib.request
 """ script that fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
