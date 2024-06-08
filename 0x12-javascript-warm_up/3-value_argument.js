084f247595789fe29f8f45dae00cc15956e54231
DanielleBadobre <109845369+DanielleBadobre@users.noreply.github.com>
2024-06-08 22:15:50 +0000

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const argmt = process.argv;
-if (!(argmt[2])) {
-  console.log('No argument');
-} else {
-  console.log(argmt[2]);
-}
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const argmt = process.argv;
-console.log(argmt[2] + ' is ' + argmt[3]);
