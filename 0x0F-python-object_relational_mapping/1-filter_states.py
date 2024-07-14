7b82612b94d639219d43ab58d053aefd826fd40f
Danielle Badobre <badobredanielle@gmail.com>
2024-07-14 18:32:35 +0000

    documentation of module

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0 starting with N"""
+"""lists all states from the database hbtn_0e_0 starting with N"""
+
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3], port=3306)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-takes in an argument and displays all values in the states table of
-hbtn_0e_0_usa
-"""
+"""takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3], port=3306)
